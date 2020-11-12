63a843764ff4a0c52cb907624c56ba4057efccff
19jy0102 <19jy0102@PC06.jynet2.jec.ac.jp>
2020-11-12 11:27:19 +0900

    ""

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -2,7 +2,7 @@ package project1;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!!");
+		System.out.println("HelloW   orld!!");
 		System.out.println("Java");
 	}
 }
